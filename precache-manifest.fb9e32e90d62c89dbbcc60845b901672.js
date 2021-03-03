9580ff8447882c459eacdc1011858bb18fb754de
coderyi <coderyi@foxmail.com>
2021-03-04 00:41:29 +0800

    Updates

---
+++ precache-manifest.fb9e32e90d62c89dbbcc60845b901672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe3714069a585015564422c8578ec64",
+    "url": "/monkeyweb/index.html"
+  },
+  {
+    "revision": "980814db8ea5b0927444",
+    "url": "/monkeyweb/static/css/main.148d6ef1.chunk.css"
+  },
+  {
+    "revision": "368a092a2b413769847f",
+    "url": "/monkeyweb/static/js/2.76b1deac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monkeyweb/static/js/2.76b1deac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980814db8ea5b0927444",
+    "url": "/monkeyweb/static/js/main.1df58b20.chunk.js"
+  },
+  {
+    "revision": "7e1733b9705352ce10f9",
+    "url": "/monkeyweb/static/js/runtime-main.5560e5a5.js"
+  }
+]);
